cab1e82c85cba266f05e9101618f68b18e052681
Tatiana Osipova <otv120780@gmail.com>
2023-01-31 14:23:59 -0500

    feat: finish slider

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const slider = document.querySelector('#slider');
 const sliderItems = Array.from(slider.children);
 const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
 
 sliderItems.forEach(function (slide, index) {
     console.log(slide);
@@ -17,40 +18,76 @@ sliderItems.forEach(function (slide, index) {
 
     // Click on the slides
     slide.addEventListener('click', function () {
+        showNextSlide('next');
 
-        // Hide current slide        
-        slide.classList.add('hidden'); 
-        slide.removeAttribute('data-active');
+        // // Hide current slide        
+        // slide.classList.add('hidden'); 
+        // slide.removeAttribute('data-active');
         
-        // Calculate the index of the next slide
-        const nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
+        // // Calculate the index of the next slide
+        // const nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
 
-        // Find next slide
-        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+        // // Find next slide
+        // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
         
-        // Show next slide
-        nextSlide.classList.remove('hidden'); 
-        nextSlide.setAttribute('data-active', '');
+        // // Show next slide
+        // nextSlide.classList.remove('hidden'); 
+        // nextSlide.setAttribute('data-active', '');
     });
 });
 
 btnNext.onclick = function() {
     console.log('Next Slide');
+    showNextSlide('next');
 
+    // // Hide current slide
+    // const currentSlide = slider.querySelector('[data-active]');
+    // const currentSlideIndex = +currentSlide.dataset.index;
+
+    // currentSlide.classList.add('hidden');
+    // currentSlide.removeAttribute('data-active');
+
+    // // Show next slide
+    // const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+    // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    // nextSlide.classList.remove('hidden');
+    // nextSlide.setAttribute('data-active', '');
+}
+
+btnPrev.onclick = function() {
+    console.log('Prev Slide');
+    showNextSlide('prev');
+
+    // // Hide current slide
+    // const currentSlide = slider.querySelector('[data-active]');
+    // const currentSlideIndex = +currentSlide.dataset.index;
+    // currentSlide.classList.add('hidden');
+    // currentSlide.removeAttribute('data-active');
+
+    // // Show next slide
+    // const nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+    // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    // nextSlide.classList.remove('hidden');
+    // nextSlide.setAttribute('data-active', '');
+};
+
+function showNextSlide(direction) {
     // Hide current slide
     const currentSlide = slider.querySelector('[data-active]');
     const currentSlideIndex = +currentSlide.dataset.index;
-
     currentSlide.classList.add('hidden');
     currentSlide.removeAttribute('data-active');
 
-    // Show next slide
-    const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+    // Calculate the next index depending on the direction 
+    let nextSlideIndex;
+    if (direction === 'next') {
+        nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+    } else if (direction === 'prev') {
+        nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+    }
+
+     // Show next slide
     const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
     nextSlide.classList.remove('hidden');
     nextSlide.setAttribute('data-active', '');
-
-
-    
-
 }
