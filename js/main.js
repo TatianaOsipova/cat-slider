d533e318da05bce25035591d9d9a2611b7b09eef
Tatiana Osipova <otv120780@gmail.com>
2023-02-11 08:49:05 -0500

    fix: delete some code

--- js/main.js
+++ js/main.js
@@ -19,56 +19,17 @@ sliderItems.forEach(function (slide, index) {
     // Click on the slides
     slide.addEventListener('click', function () {
         showNextSlide('next');
-
-        // // Hide current slide        
-        // slide.classList.add('hidden'); 
-        // slide.removeAttribute('data-active');
-        
-        // // Calculate the index of the next slide
-        // const nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
-
-        // // Find next slide
-        // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-        
-        // // Show next slide
-        // nextSlide.classList.remove('hidden'); 
-        // nextSlide.setAttribute('data-active', '');
     });
 });
 
 btnNext.onclick = function() {
     console.log('Next Slide');
     showNextSlide('next');
-
-    // // Hide current slide
-    // const currentSlide = slider.querySelector('[data-active]');
-    // const currentSlideIndex = +currentSlide.dataset.index;
-
-    // currentSlide.classList.add('hidden');
-    // currentSlide.removeAttribute('data-active');
-
-    // // Show next slide
-    // const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
-    // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-    // nextSlide.classList.remove('hidden');
-    // nextSlide.setAttribute('data-active', '');
 }
 
 btnPrev.onclick = function() {
     console.log('Prev Slide');
     showNextSlide('prev');
-
-    
-    // const currentSlide = slider.querySelector('[data-active]');
-    // const currentSlideIndex = +currentSlide.dataset.index;
-    // currentSlide.classList.add('hidden');
-    // currentSlide.removeAttribute('data-active');
-
-    // // Show next slide
-    // const nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
-    // const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-    // nextSlide.classList.remove('hidden');
-    // nextSlide.setAttribute('data-active', '');
 };
 
 function showNextSlide(direction) {
